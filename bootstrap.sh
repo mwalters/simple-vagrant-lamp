f2b0566a73d85048d78029b3904d70c7e210ea57
Matt Walters <me@mattwalters.net>
2013-07-25 15:39:04 -0400

    Adds timezone setting to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,11 @@ sudo apt-get update
 sudo apt-get -y install mysql-server-5.5 php5-mysql libsqlite3-dev apache2 php5 php5-dev build-essential php-pear
 
 
+# Set timezone
+echo "America/New_York" | sudo tee /etc/timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+
+
 # Setup database
 if [ ! -f /var/log/databasesetup ];
 then
